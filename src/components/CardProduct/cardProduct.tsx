9fc63d762c2718d4f44bc1863a05e55f67594e24
hvivox <hvivox@hotmail.com>
2023-09-10 00:21:29 -0400

    AXIOS | utilizando o axios primeiro acesso ao jsonplaceholder

--- src/components/CardProduct/cardProduct.tsx
+++ src/components/CardProduct/cardProduct.tsx
@@ -1,22 +1,35 @@
 //import "bootstrap/dist/css/bootstrap.css"; // Importe o CSS do Bootstrap
+import axios from 'axios';
 
 type Props = {
-  name: string;
-  price: number;
-  photo: string;
+    name: string;
+    price: number;
+    photo: string;
 };
 
 export const CardProduct = (props: Props) => {
-  const modalCadastro = () => {
-    alert("abertura modal");
-  };
+    const modalCadastro = () => {
+        alert("abertura modal");
+    };
 
-  return (
-    <div className="m-4">
-      <h1>{props.name}</h1>
-      <button className="btn btn-primary" onClick={modalCadastro}>
-        cadastrar
-      </button>
-    </div>
-  );
+    const handleGetPosts = () => {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then( (response)=>{
+                console.log(response.data);
+            });
+    }
+
+    return (
+        <div className="m-4">
+            <h1>{props.name}</h1>
+            <button className="btn btn-primary" onClick={modalCadastro}>
+                cadastrar
+            </button>
+
+            <button className="btn btn-primary" onClick={handleGetPosts}>
+                Pegar posts
+            </button>
+
+        </div>
+    );
 };
