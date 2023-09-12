1edda9160d063c5ed3198f362618857a0ee229bd
hvivox <hvivox@hotmail.com>
2023-09-12 00:19:30 -0400

    campo input passa dados para o modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,37 @@
 import "./App.css";
 import { CardProduct } from "./components/CardProduct/cardProduct.tsx";
-import {ListaFiltradaOrdenada} from "./components/ListaFiltradaOrdenada/ListaFiltradaOrdenada.tsx";
-import {ModalPrincipal} from "./components/ModalPrincipal/ModalPrincipal.tsx"; // Importe o CSS do Bootstrap
-
+import { FildInsert } from "./components/FieldInsert/FildInsert.tsx";
+import { ListaFiltradaOrdenada } from "./components/ListaFiltradaOrdenada/ListaFiltradaOrdenada.tsx";
+import { ModalPrincipal } from "./components/ModalPrincipal/ModalPrincipal.tsx"; // Importe o CSS do Bootstrap
 
 function App() {
   const getWeekDay = () => {
     return new Intl.DateTimeFormat("pt-BR", { weekday: "long" }).format(new Date());
   };
 
+  const handleAddTask = (taskName: string) => {
+    alert(taskName);
+    /* let newList = [...list];
+      newList.push({
+        id: list.length + 1,
+        name: taskName,
+        done: false,
+      });
+      setList(newList);*/
+  };
 
   return (
     <div className="App">
       <div className="container">
         <h3>{getWeekDay()}</h3>
-       <ModalPrincipal nameButtonModal="Lista de cartões">
-           <CardProduct name="xmogenes" price={10.42} photo="testeeststeste" />
-           <CardProduct name="hmsilva" price={18.42} photo="link da imagem" />
-       </ModalPrincipal>
+        <ModalPrincipal nameButtonModal="Lista de cartões">
+          <CardProduct name="xmogenes" price={10.42} photo="testeeststeste" />
+          <CardProduct name="hmsilva" price={18.42} photo="link da imagem" />
+        </ModalPrincipal>
 
-        <ListaFiltradaOrdenada/>
+        <ListaFiltradaOrdenada />
+
+        <FildInsert onEnter={handleAddTask} />
       </div>
     </div>
   );
--- src/components/CardProduct/cardProduct.tsx
+++ src/components/CardProduct/cardProduct.tsx
@@ -1,54 +1,45 @@
 //import "bootstrap/dist/css/bootstrap.css"; // Importe o CSS do Bootstrap
-import {api} from "../../utils/api.ts";
-import {useEffect, useState} from "react";
+import { api } from "../../utils/api.ts";
+import { useEffect, useState } from "react";
 
 type Props = {
-    name: string;
-    price: number;
-    photo: string;
+  name: string;
+  price: number;
+  photo: string;
 };
 
 interface Product {
-    userId: number;
-    id: number;
-    title: string;
-    body: string;
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
 }
 
-
 export const CardProduct = (props: Props) => {
-    const [Product, setProduct] = useState<Product[]>([]);
-    const [isLoading, setIsLoading] = useState(true);
+  const [Product, setProduct] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    //INSERE DADOS NO COMPONENTE NO MOMENTO DA INICIALIZAÇÃO DO MESMO
-    useEffect( () => {
-        /*async  function fetchData(){
-            const _products = await handleGetPosts();
-            setProduct(_products);
-        }*/
-
-
-        //CICLO DE VIDA: utilizado para carregar o objeto no momento da inicialização
-        async function fetchData() {
-            try {
-                const response = await api.get<Product[]>('/posts');
-                setProduct(response.data);
-                setIsLoading(false);
-            } catch (error) {
-                console.error('Erro ao buscar dados:', error);
-                setIsLoading(false);
-            }
-        }
-        fetchData()
-    }, [])
-
-
-    if (isLoading) {
-        return <div>Carregando...</div>;
+  //INSERE DADOS NO COMPONENTE NO MOMENTO DA INICIALIZAÇÃO DO MESMO
+  useEffect(() => {
+    //CICLO DE VIDA: utilizado para carregar o objeto no momento da inicialização
+    async function fetchData() {
+      try {
+        const response = await api.get<Product[]>("/posts");
+        setProduct(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error("Erro ao buscar dados:", error);
+        setIsLoading(false);
+      }
     }
+    fetchData();
+  }, []);
 
+  if (isLoading) {
+    return <div>Carregando...</div>;
+  }
 
-   /* const handleGetPosts = () => {
+  /* const handleGetPosts = () => {
         api.get<Product[]>('/posts')
             .then((response) => {
                 setProduct(response.data);
@@ -61,32 +52,27 @@ export const CardProduct = (props: Props) => {
     }
 */
 
-    const modalCadastro = () => {
-        alert("abertura modal");
-    };
+  const modalCadastro = () => {
+    alert("abertura do alert");
+  };
 
-
-    return (
-        <div className="m-4">
-
-            <div>
-                <h2>Dados da API:</h2>
-                <ul>
-                    {Product.map((item) => (
-                        <li key={item.id} >{item.title} | {item.body}</li>
-                    ))}
-                </ul>
-            </div>
-
-            <hr/>
-
-            <h1>{props.name}</h1>
-            <button className="btn btn-primary" onClick={modalCadastro}>
-                cadastrar
-            </button>
-
-
-
-        </div>
-    );
+  return (
+    <div className="m-4">
+      <div>
+        <h2>Dados da API:</h2>
+        <h1>{props.name}</h1>
+        <ul>
+          {Product.map((item) => (
+            <li key={item.id}>
+              {item.title} | {item.body}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <button className="btn btn-primary" onClick={modalCadastro}>
+        cadastrar
+      </button>
+      <hr />
+    </div>
+  );
 };
